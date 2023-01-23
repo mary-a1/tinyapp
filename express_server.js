546c0fa0f0667395752c3af1e9fe28c340ac5e65
Maryan <maryanay2000@gmail.com>
2023-01-23 18:55:51 -0500

    Adding new files

--- express_server.js
+++ express_server.js
@@ -3,11 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,6 +31,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id
   console.log(urlDatabase[id]);
@@ -35,3 +44,11 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2,8);
+  console.log(result);
+}
+
+generateRandomString();
